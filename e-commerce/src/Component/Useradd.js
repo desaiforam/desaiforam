2f2388be1eead6956269899f41c0620de410b23d
desaiforam <FP406758@gmail.com>
2024-02-16 10:35:50 +0530

    intitial

--- e-commerce/src/Component/Useradd.js
+++ e-commerce/src/Component/Useradd.js
@@ -1,111 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import Navebar from './Navebar'
-import Header from './header'
-import Footer from './Footer'
-import { useDispatch, useSelector } from 'react-redux'
-import AddtwoCart from './AddtwoCart'
-import { useLocation, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import Navebar from "./Navebar";
+import Header from "./header";
+import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import AddtwoCart from "./AddtwoCart";
+import { useLocation, useNavigate } from "react-router-dom";
 // import AuthAction from '../store/action/AuthAction'
 
 const Useradd = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-   
-    const { state } = useLocation()
-  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  console.log("dispatch", dispatch);
 
+  const { state } = useLocation();
 
-    const { advocaat} = useSelector((state) => state.Auth)
-    console.log('advocaat', advocaat);
-    const [SubTotal, setSubTotal] = useState([])
-    const [total, settotal] = useState(0)
-    const proceedToCheckout = () => {
+  const { advocaat } = useSelector((state) => state.Auth);
+  console.log("advocaat", advocaat);
+  const [SubTotal, setSubTotal] = useState([]);
+  const [Total, setTotal] = useState(0);
+  const proceedToCheckout = () => {
+    navigate("/Cart-Details ", { state: SubTotal });
+  };
+  const gasprice = () => {
+    const pricetotal = advocaat.map((item) => ({
+      id: item.id,
+      price: item.price,
+    }));
+    const totalData = pricetotal.reduce((accumulator, object) => {
+      return accumulator + object.price;
+    }, 0);
+    setTotal(totalData);
+    setSubTotal(pricetotal);
+  };
 
-        navigate("/Cart-Details ", { state: SubTotal })
-    }
-    const gasprice = () => {
-        const pricetotal = advocaat.map(item => ({ id: item.id, price: item.price }))
-        const totalData = pricetotal.reduce((accumulator, object) => {
+  useEffect(() => {
+    gasprice();
+  }, [state]);
+  console.log("state", state);
 
-            return accumulator + object.price;
+  const onhandalprice = (index, qty, itemize) => {
+    const data = [...SubTotal];
+    data[index].price = itemize * qty;
+    setSubTotal(data);
 
-        }, 0);
-        settotal(totalData)
-        setSubTotal(pricetotal)
-    }
+    const totalData = data.reduce((accumulator, object) => {
+      return accumulator + object.price;
+    }, 0);
 
-    useEffect(() => {
-        gasprice()
-    },[state])
+    setTotal(totalData);
+  };
 
-    const onhandalprice = (index, qty, itemprice) => {
-        const data = [...SubTotal]
-        data[index].price = itemprice * qty
-        setSubTotal(data)
-
-        const totalData = data.reduce((accumulator, object) => {
-            return accumulator + object.price;
-        }, 0);
-
-        settotal(totalData)
-    }
-
-    return (
-        <div>
-            <Navebar />
-            <Header />
-            <div className='container d-flex flex-column '>
-                <div className='homecart'>Home/ Cart</div>
-                <div className='tablecatr'>
-                    <tr className='cartable'>
-                        <td className='cartages'>Product </td>
-                        <td>Price</td>
-                        <td>Quantity</td>
-                        <td>Subtotal</td>
-                    </tr>
-                    {advocaat&&
-                        advocaat.map((item, index) => {
-                            // const totalprice = state.find((o) => {
-                            //     return o.id === item.id})
-                            return <><AddtwoCart item={item} index={index} onhandalprice={(quantity) => onhandalprice(index, quantity, item.price)} />
-                            </>
-                        })}
-                </div>
-                <div className='btn btngroup d-flex' >
-                    <button>Return To Shop </button>
-                    <button>Update Cart</button>
-                </div>
-                <div className='applycoupon d-flex flex-row justify-content-between'>
-                    <div className="codecoupon "><input className='inputted' type='text' name='Coupon Code' placeholder='Coupon Code' />
-                        <button className='coupon'>Apply Coupon</button>
-                    </div>
-                    <div className='carttotal d-flex flex-column '>
-                        <span className='total mb-3'>Cart Total</span>
-                        <div className='pricetotal d-flex flex-row justify-content-between'>
-                            <span>Subtotal:</span>
-                            {Number(total).toFixed(2)}
-                        </div>
-                        <hr w-75 />
-                        <div className='pricetotal d-flex flex-row  justify-content-between'>
-                            <span>Shipping:</span>
-                            <span>Free</span>
-                        </div>
-                        <hr></hr>
-                        <div className='pricetotal d-flex flex-row justify-content-between'>
-                            <span >Total:</span>
-                            {Number(total).toFixed(2)}
-                        </div>
-                        <div className='process d-flex justify-content-center'>
-                            <div onClick={proceedToCheckout} style={{ cursor: "pointer" }} className='position-relative'>
-                                <button className='coupon mt-4 '>Procees to checkout</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer />
+  return (
+    <div>
+      <Navebar />
+      <Header />
+      <div className="container d-flex flex-column ">
+        <div className="homers">Home/ Cart</div>
+        <div className="tablet">
+          <tr className="cartable">
+            <td className="cartages">Product </td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Subtotal</td>
+          </tr>
+          {advocaat &&
+            advocaat.map((item, index) => {
+              return (
+                <>
+                  <AddtwoCart
+                    item={item}
+                    index={index}
+                    onhandalprice={(quantity) =>
+                      onhandalprice(index, quantity, item.price)
+                    }
+                  />
+                </>
+              );
+            })}
         </div>
-    )
-}
+        <div className="btn btngroup d-flex">
+          <button>Return To Shop </button>
+          <button>Update Cart</button>
+        </div>
+        <div className="applycoupon d-flex flex-row justify-content-between">
+          <div className="codecoupon ">
+            <input
+              className="inputted"
+              type="text"
+              name="Coupon Code"
+              placeholder="Coupon Code"
+            />
+            <button className="coupon">Apply Coupon</button>
+          </div>
+          <div className="carlotta d-flex flex-column ">
+            <span className="total mb-3">Cart Total</span>
+            <div className="pricetotal d-flex flex-row justify-content-between">
+              <span>Subtotal:</span>
+              {Number(Total).toFixed(2)}
+            </div>
+            <hr w-75 />
+            <div className="pricetotal d-flex flex-row  justify-content-between">
+              <span>Shipping:</span>
+              <span>Free</span>
+            </div>
+            <hr></hr>
+            <div className="pricetotal d-flex flex-row justify-content-between">
+              <span>Total:</span>
+              {Number(Total).toFixed(2)}
+            </div>
+            <div className="process d-flex justify-content-center">
+              <div
+                onClick={proceedToCheckout}
+                style={{ cursor: "pointer" }}
+                className="position-relative"
+              >
+                <button className="coupon mt-4 ">Proceed to checkout</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Useradd
+export default Useradd;
